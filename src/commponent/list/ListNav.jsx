65c6eaa5c420be4ef282451e5da0d303e703b6e1
baedollee <baesee0401@gmail.com>
2022-08-19 20:13:23 +0900

    첫날 뷰 그냥 틀만

--- src/commponent/list/ListNav.jsx
+++ src/commponent/list/ListNav.jsx
@@ -5,7 +5,7 @@ const ListNav = () => {
   return (
     <div className="ListNav_Wrap">
       <div className="ListNav_right">
-        <div>프로필 이미지</div>
+        <div className="ListNav_img">프로필 이미지</div>
         <div>닉네임</div>
       </div>
       <button>설정버튼</button>
