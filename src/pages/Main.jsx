ed687bcb5c93c553ddd2058edc339d6c1a4aeb22
Yoonwon <blacksyw@naver.com>
2022-08-20 02:34:32 +0900

    yoonwon/DetailPage css 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import List from "../commponent/list/List";
 import Header from "../commponent/header/Header";
-import Login from "../commponent/login/login";
+import Login from "../commponent/login/Login";
 import { postsList } from "../redux/modules/postsSlice";
 import { useSelector } from "react-redux";
 
