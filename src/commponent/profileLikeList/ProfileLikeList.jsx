74d90f215cabb0fed6b1c603711e17573d8531e4
Yoonwon <blacksyw@naver.com>
2022-08-20 20:14:02 +0900

    yoonwon Profile 호버빼고 완성

---
+++ src/commponent/profileLikeList/ProfileLikeList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./ProfileLikeList.scss";
+
+const ProfileLikeList = () => {
+  return (
+    <div className="ProfileLikeListContainer">
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/4a/38/47/4a384760af76f4488c77013bb1ea7607.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/a3/3d/a0/a33da0414862a5083eafb61aac672eb2.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/45/5d/85/455d85fe373a870334be97aee28e278a.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/db/30/f5/db30f5a003b8856ab0a5065f8a0ee42f.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/28/a6/81/28a681c02ef14e1b0d92555a73def034.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/cf/8d/d1/cf8dd153c0b434badaccf9a0023e436c.jpg"
+      />
+      <img
+        className="ProfileLikeListImg"
+        src="https://i.pinimg.com/236x/ab/99/a7/ab99a78ddf9fed2cb85f3eda4fe73a60.jpg"
+      />
+    </div>
+  );
+};
+
+export default ProfileLikeList;
--- src/commponent/profileList/ProfileList.jsx
+++ src/commponent/profileList/ProfileList.jsx
@@ -1,60 +1,65 @@
 import React, { useState } from "react";
 import "./ProfileList.scss";
 import { FaTh, FaHeart } from "react-icons/fa";
+import ProfilePostList from "../profilePostList/ProfilePostList";
+import ProfileLikeList from "../profileLikeList/ProfileLikeList";
 
 const ProfileList = () => {
   const [choice, setChoice] = useState(1);
   return (
-    <div className="ProfileBottomContainer">
-      <div className="ProfileBottomNavBar">
-        {choice === 1 ? (
-          <>
-            {" "}
-            <div className="ProfileBottomNavBarPost1">
-              <div className="ProfileIconMargin">
-                <div className="ProfileBottomNavBarIcon">
-                  <FaTh />
+    <>
+      <div className="ProfileBottomContainer">
+        <div className="ProfileBottomNavBar">
+          {choice === 1 ? (
+            <>
+              {" "}
+              <div className="ProfileBottomNavBarPost1">
+                <div className="ProfileIconMargin">
+                  <div className="ProfileBottomNavBarIcon">
+                    <FaTh />
+                  </div>
+                  &nbsp; 게시물
                 </div>
-                &nbsp; 게시물
               </div>
-            </div>
-            <div
-              className="ProfileBottomNavBarLike1"
-              onClick={() => setChoice(2)}
-            >
-              <div className="ProfileIconMargin">
-                <div className="ProfileBottomNavBarIcon">
-                  <FaHeart />
+              <div
+                className="ProfileBottomNavBarLike1"
+                onClick={() => setChoice(2)}
+              >
+                <div className="ProfileIconMargin">
+                  <div className="ProfileBottomNavBarIcon">
+                    <FaHeart />
+                  </div>
+                  &nbsp;좋아요
                 </div>
-                &nbsp;좋아요
               </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div
-              className="ProfileBottomNavBarPost2"
-              onClick={() => setChoice(1)}
-            >
-              <div className="ProfileIconMargin">
-                <div className="ProfileBottomNavBarIcon">
-                  <FaTh />
+            </>
+          ) : (
+            <>
+              <div
+                className="ProfileBottomNavBarPost2"
+                onClick={() => setChoice(1)}
+              >
+                <div className="ProfileIconMargin">
+                  <div className="ProfileBottomNavBarIcon">
+                    <FaTh />
+                  </div>
+                  &nbsp; 게시물
                 </div>
-                &nbsp; 게시물
               </div>
-            </div>
-            <div className="ProfileBottomNavBarLike2">
-              <div className="ProfileIconMargin">
-                <div className="ProfileBottomNavBarIcon">
-                  <FaHeart />
+              <div className="ProfileBottomNavBarLike2">
+                <div className="ProfileIconMargin">
+                  <div className="ProfileBottomNavBarIcon">
+                    <FaHeart />
+                  </div>
+                  &nbsp;좋아요
                 </div>
-                &nbsp;좋아요
               </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+      {choice === 1 ? <ProfilePostList /> : <ProfileLikeList />}
+    </>
   );
 };
 
