e9741849a924f9bcae120a189854721117cba4bb
baedollee <baesee0401@gmail.com>
2022-08-22 09:35:58 +0900

    header css 완성

--- src/commponent/header/Header.jsx
+++ src/commponent/header/Header.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
 import './Header.scss';
 import logo from '../../img/logo.png';
-import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
-
+import { MdOutlineKeyboardArrowDown, MdHomeFilled } from 'react-icons/md';
+import { BiSearch } from 'react-icons/bi';
+import { BsPlusSquare, BsHeart } from 'react-icons/bs';
 const Header = () => {
   return (
-    <div className="wrap">
-      <div className="container_left">
-        <img className="container_left_logo_img" src={logo} alt="" />
-        <button className="container_left_btn">
-          <MdOutlineKeyboardArrowDown />
-        </button>
+    <div className="header_wrap">
+      <div className="header_container">
+        <div className="container_left">
+          <img className="container_left_logo_img" src={logo} alt="" />
+          <button className="container_left_btn">
+            <MdOutlineKeyboardArrowDown />
+          </button>
+        </div>
+        <div className="container_middle" role="button">
+          <input
+            className="container_middle_wrap"
+            placeholder="검색"
+            type="text"
+          />
+          {/* <div className="container_middle_box_search">
+            <BiSearch className="container_middle_box_search_imo" />
+          </div> */}
+        </div>
+        <div className="container_right">
+          <div className="container_right_wrap">
+            <div className="container_right_wrap _home">
+              <MdHomeFilled className="container_right_home _home" />
+            </div>
+            <div className="container_right_wrap _plus">
+              <BsPlusSquare className="container_right_plus _plus" />
+            </div>
+            <div className="container_right_wrap _heart">
+              <BsHeart className="container_right_home _heart" />
+            </div>
+            <div className="container_right_wrap _img">
+              <img
+                className="container_right_img _img"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/IU_posing_for_Marie_Claire_Korea_March_2022_issue_03.jpg/500px-IU_posing_for_Marie_Claire_Korea_March_2022_issue_03.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="container_middle" role="button" tabIndex="0">
-        <button className="container_middle_box" placeholder="검색"></button>
-      </div>
-      <div className="container_right"></div>
     </div>
   );
 };
--- src/commponent/list/ListContent.jsx
+++ src/commponent/list/ListContent.jsx
@@ -8,7 +8,7 @@ import {
   BsHeart,
   BsEmojiSmile,
 } from 'react-icons/bs';
-const ListContent = () => {
+const ListContent = ({ listData }) => {
   return (
     <div className="ListContentWrap">
       <div className="ListContentHead">
@@ -31,20 +31,20 @@ const ListContent = () => {
 
       <div className="ListContentBody">
         <h3 className="ListContentBodyLike">
-          좋아요 <span>1000</span>개
+          좋아요 <span>&nbsp;{listData.postLikeCnt}</span>개
         </h3>
 
         <div className="ListContentBodyComment">
-          <h3 className="ListContentBodyCommentNickname">배돌이</h3>
+          <h3 className="ListContentBodyCommentNickname">{listData.author}</h3>
           <div className="ListContentBodyCommentWrap">
             <span className="ListContentBodyCommentWrapText">
-              오늘은 무얼하고 놀아야 할까
+              {listData.content}&nbsp;
             </span>
             <button>더 보기</button>
           </div>
         </div>
         <div className="ListContentBodyComment_reply">
-          댓글 <span> 9</span>개 모두 보기
+          댓글&nbsp;<span>{listData.commentCnt}</span>개 모두 보기
         </div>
 
         <div className="ListContentBody_reply_wrap">
