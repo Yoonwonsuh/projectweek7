ba368e301880d314bf8bc5d96b986a2703e3196e
yoonwon <blacksyw@naver.com>
2022-08-23 15:04:20 +0900

    yoonwon Delete추가
    메인페이지 ... 모달창 추가 및 삭제 기능 추가

--- src/commponent/postModal/PostModal.jsx
+++ src/commponent/postModal/PostModal.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { deletePostsList } from "../../redux/modules/postsSlice";
 import "./PostModal.scss";
 
 const PostModal = ({ListData}) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="postModalContainer">
       <div className="postModalEditBox">
         <div className="postModalEditText">수정</div>
       </div>
       <div className="postModalDeleteBox">
-        <div className="postModalDeleteText">삭제</div>
+        <div className="postModalDeleteText" onClick={()=>dispatch(deletePostsList(ListData.postId))}>삭제</div>
       </div>
     </div>
   );
